08647004d8962cea67d89280833f048f0d83f6e7
Ferry <ferrygreen@outlook.com>
2016-10-20 16:47:26 +0800

    添加新文件

---
+++ fileout/src/fileout/FileIO.java
@@ -0,0 +1,20 @@
+package fileout;
+
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+public class FileIO {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO 自动生成的方法存根
+		FileOutputStream FileIOTest= new FileOutputStream("test.txt",true);
+		BufferedOutputStream OutTem = new BufferedOutputStream(FileIOTest);
+		outtem
+
+ 
+		
+
+	}
+
+}
